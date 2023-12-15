7cf19069d6352e67b01b1540b60cd09cbb7678cd
Steven Johnson <stjohnson@jaspergroup.us.com>
2023-12-15 13:24:39 -0500

    formatted day 6

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,15 +1,15 @@
+mod helper_functions;
 mod pt1;
 mod pt2;
-mod helper_functions;
 
 fn main() {
     match pt1::answer("./day6_input/input.txt") {
         Ok(answer) => println!("Pt1. Answer: {}", answer),
-        Err(e) => println!("{}", e)
+        Err(e) => println!("{}", e),
     }
 
-    match pt2:: answer("./day6_input/input.txt") {
+    match pt2::answer("./day6_input/input.txt") {
         Ok(answer) => println!("Pt2. Answer: {}", answer),
-        Err(e) => println!("{}", e)
+        Err(e) => println!("{}", e),
     }
 }
