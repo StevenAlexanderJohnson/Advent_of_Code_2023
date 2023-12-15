3796d7161e67705322d435cec4c63a6c2690dbcb
Steven Johnson <stjohnson@jaspergroup.us.com>
2023-12-15 13:34:56 -0500

    simplified answer functions

--- day6/src/pt1.rs
+++ day6/src/pt1.rs
@@ -1,16 +1,15 @@
 use crate::helper_functions::{find_race_error_margin, parse_input_into_races, read_input_file};
 
 pub fn answer(input_path: &str) -> Result<u64, &'static str> {
-    let input: Vec<String> = match read_input_file(input_path) {
-        Ok(file_data) => file_data,
-        Err(e) => return Err(e),
-    };
-
-    if input.len() != 2 {
-        return Err("Error when validating input file parse.");
-    }
-
-    let error_margin: u64 = parse_input_into_races(input)?
+    let error_margin = read_input_file(input_path)
+        .and_then(|input| {
+            if input.len() != 2 {
+                Err("Error when validating input file parse.")
+            } else {
+                Ok(input)
+            }
+        })
+        .and_then(|x| parse_input_into_races(x))?
         .iter()
         .map(|race| find_race_error_margin(race))
         .fold(1, |x, y| x * y);
--- day6/src/pt2.rs
+++ day6/src/pt2.rs
@@ -1,17 +1,17 @@
 use crate::helper_functions::{
-    find_race_error_margin, read_input_file, remove_kerning_and_parse_race, RaceInfo,
+    find_race_error_margin, read_input_file, remove_kerning_and_parse_race,
 };
 
 pub fn answer(input_path: &str) -> Result<u64, &'static str> {
-    let input: Vec<String> = read_input_file(input_path)?;
-
-    if input.len() != 2 {
-        return Err("Error when validating input file parse.");
-    }
-
-    let race: RaceInfo = remove_kerning_and_parse_race(input)?;
-
-    let error_margin: u64 = find_race_error_margin(&race);
+    let error_margin: u64 = read_input_file(input_path)
+        .and_then(|input| {
+            if input.len() != 2 {
+                Err("Error when validating input file parse.")
+            } else {
+                remove_kerning_and_parse_race(input)
+            }
+        })
+        .and_then(|race| Ok(find_race_error_margin(&race)))?;
 
     Ok(error_margin)
 }
